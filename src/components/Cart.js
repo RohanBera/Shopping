570185f4d125aa5e8f9a0e232dc70f7775e94a4e
RohanBera <berarohan88@gmail.com>
2021-01-17 22:29:03 +0530

    Main cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,116 +4,70 @@ import Fade from 'react-reveal/Fade'
 export default class Cart extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            name: "",
-            email: "",
-            address: "",
-            showCheckout: false,
+        this.state={
+            cartItems: JSON.parse(localStorage.getItem('cartItems')) ? JSON.parse(localStorage.getItem('cartItems')) : []
         }
     }
 
-    handleChange = (event) => {
-        var { name, value } = event.target
-        this.setState({
-            [name]: value,
-        })
+    removeFromCart = (product) => {
+		const cartItems = this.state.cartItems.slice()
+		const updatedItems = cartItems.filter(item => item._id !== product._id)
+		this.setState({
+			cartItems : updatedItems, 
+		})
+		localStorage.setItem('cartItems', JSON.stringify(updatedItems))
     }
-
-    placeOrder = (event) => {
-        event.preventDefault()
-
-        const order = {
-            name: this.state.name,
-            email: this.state.email,
-            address: this.state.address,
-            cartItem: this.props.cartItems,
-        }
-
-        this.props.placeOrder(order);
-    }
-
+    
     render() {
-        const { cartItems } = this.props
+        const cartItems =  this.state.cartItems
+        var total = cartItems.reduce((total, item) => (total + item.price * item.count), 0)
         return (
-            <div>
-                {cartItems.length === 0 ? 
-                    <div className="cart cart-header">Cart is Empty</div> :
-                    <div className="cart cart-header">{cartItems.length} items in cart</div>
-                }
-                <div className="cart">
-                    <Fade left cascade >
-                    <ul className="cart-items">
-                        {cartItems.map(cartItem => (
-                            <li key={cartItem._id}>
-                                <div>
-                                    <img src={cartItem.image} alt={cartItem.title} />
-                                </div>
-                                <div>
-                                    {/* <div>{cartItem.title}</div> */}
-                                    <div className="right">
-                                        {"\u20B9  "+ cartItem.price} x {cartItem.count + "  "} 
-                                        <button className="button" onClick={() => this.props.removeFromCart(cartItem)}>
-                                            Remove
-                                        </button>
-                                    </div>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                    </Fade>
-                </div>
-                {cartItems.length !==0 && (
-                    <>  
-                        <div className="cart"> 
-                            <div className="total">
-                                {"Total : \u20B9  " + cartItems.reduce((total, item) => (total + item.price * item.count), 0)}
-                                <button className="button primary" onClick={()=> this.setState({showCheckout: true})}>
-                                Proceed
-                                </button>
+            <div className="main-cart">
+                <Fade left cascade >
+                <ul className="main-cart-items">
+                    {cartItems.length === 0 ? 
+                    // if cart empty, continue chopping button, back to home page
+                        <div className="main-cart-header">Cart is Empty</div> :
+                        <div className="main-cart-header"><b>My Cart </b>{" ("+cartItems.length} items)</div>
+                    }
+                    {cartItems.map(cartItem => (
+                        <li key={cartItem._id}>
+                            <div>
+                                <img src={cartItem.image} alt={cartItem.title} />
                             </div>
+                            <div>
+                                <div className="main-cart-item-title">{cartItem.title}</div>
+                                <div>{"Quantity:  " + cartItem.count}</div>
+                                <div className="right">
+                                    {"\u20B9  "+ cartItem.price} x {cartItem.count + "  "} 
+                                    <button className="button" onClick={() => this.removeFromCart(cartItem)}>
+                                        Remove
+                                    </button>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                </Fade>
+                {cartItems.length !==0 && (
+                    <div className="main-cart-total">
+                        <div className="row">
+                            <span>{"Price (" + cartItems.length + "items) : "}</span> 
+                            <span>{"\u20B9  " + total}</span>
+                        </div>
+                        <div className="row">
+                            <span>{"Delivery Charges : "}</span> 
+                            <span>{"\u20B9  120"}</span>
+                        </div>
+                        <div className="row final">
+                            <span>{"Total : "}</span> 
+                            <span>{"\u20B9  " + (total + 120) }</span>
                         </div>
 
-                        {this.state.showCheckout && (
-                                <Fade right cascade >
-                                <div className="cart">
-                                <form onSubmit={this.placeOrder}>
-                                    <ul className="form-container">
-                                        <li>
-                                            <lable>Name</lable>
-                                            <input 
-                                                name="name" 
-                                                type="text"
-                                                required
-                                                onChange={this.handleChange}
-                                            />
-                                        </li>
-                                        <li>
-                                            <lable>Email</lable>
-                                            <input 
-                                                name="email"
-                                                type="email"
-                                                required
-                                                onChange={this.handleChange}
-                                            />
-                                        </li>
-                                        <li>
-                                            <lable>Address</lable>
-                                            <input 
-                                                name="address"
-                                                type="text"
-                                                required
-                                                onChange={this.handleChange}
-                                            />
-                                        </li>
-                                        <li>
-                                            <button className="button primary" type="submit">Checkout</button>
-                                        </li>
-                                    </ul>
-                                </form>
-                            </div>
-                            </Fade>
-                        )} 
-                    </>
+                        <button className="button primary" onClick={()=> this.setState({showCheckout: true})}>
+                            Proceed
+                        </button>
+                    </div>
                 )}
             </div>
         )
